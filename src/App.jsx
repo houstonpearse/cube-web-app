54000ea47f79ed848bc816c31b83879100d17076
houy <67390187+houstonpearse@users.noreply.github.com>
2023-02-23 17:34:34 +1100

    changed lights to spotlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import {
+  OrbitControls,
+  ContactShadows,
+  Environment,
+  Stage,
+} from "@react-three/drei";
 import Cube from "./components/Cube";
 import Lights from "./components/Lights";
 
 function App() {
   return (
-    <Canvas>
-      <color attach="background" args={["black"]} />
-      <Stars />
+    <Canvas shadows camera={{ position: [2, 2, 2] }}>
+      <color attach="background" args={["white"]} />
+
+      <Cube position={[0, 0, 0]} args={[1, 1, 1]} />
       <Lights />
-      <OrbitControls />
-      <Cube />
+      <ContactShadows
+        position={[0, -1.5, 0]}
+        opacity={0.75}
+        scale={10}
+        blur={2.5}
+        far={4}
+      />
+      <Environment preset="city" />
+
+      <OrbitControls
+        makeDefault
+        minPolarAngle={Math.PI / 4}
+        maxPolarAngle={(5 * Math.PI) / 8}
+      />
     </Canvas>
   );
 }
--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -3,13 +3,11 @@ import { useHelper } from "@react-three/drei";
 import { DirectionalLightHelper } from "three";
 
 export default function Lights() {
-  const light = useRef();
-  useHelper(false && light, DirectionalLightHelper, "cyan");
   return (
     <group>
       <ambientLight intensity={0.5} />
-      <directionalLight ref={light} position={[3, 4, 5]} />
-      <directionalLight ref={light} position={[-3, -3, -1]} />
+      <spotLight position={[3, 4, 5]} />
+      <spotLight position={[-3, -3, -1]} />
     </group>
   );
 }
