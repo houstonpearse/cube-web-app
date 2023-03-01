cd13b46412d7e64d0447f00de8885ef69ab76b5c
houy <67390187+houstonpearse@users.noreply.github.com>
2023-03-01 13:15:05 +1100

    Cleaned spring usage

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,45 +1,25 @@
 import { useState } from "react";
 import { animated, useSpring, useSpringValue } from "@react-spring/three";
 
-const defaultcolors = ["red", "#f7aa36", "white", "yellow", "blue", "#6af752"];
-const randomAngle = () => {
-  return Math.random() * Math.PI - Math.PI / 2;
-};
+const defaultColors = ["red", "#f7aa36", "white", "yellow", "blue", "#6af752"];
+const springConfig = { mass: 1, friction: 50, tension: 200 };
+const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
+
 export default function Cube({
   startingposition = [0, 0, 0],
   startingrotation = [0, 0, 0],
   startingscale = 1,
-  colors = defaultcolors,
+  colors = defaultColors,
 }) {
-  const rotation = useSpringValue(startingrotation, {
-    config: {
-      mass: 1,
-      friction: 50,
-      tension: 170,
-    },
-  });
-
-  const position = useSpringValue(startingposition, {
-    config: {
-      mass: 1,
-      friction: 50,
-      tension: 170,
-    },
-  });
-
-  const scale = useSpringValue(startingscale, {
-    config: {
-      mass: 1,
-      friction: 50,
-      tension: 170,
-    },
-  });
+  const rotation = useSpringValue(startingrotation, { config: springConfig });
+  const position = useSpringValue(startingposition, { config: springConfig });
+  const scale = useSpringValue(startingscale, { config: springConfig });
 
   const handleClick = () => {
     console.log("animate");
     rotation.start([randomAngle(), randomAngle(), randomAngle()]);
     position.start([Math.random(), Math.random(), Math.random()]);
-    scale.start(Math.random() + 0.1);
+    scale.start(Math.random() + 0.5);
   };
   return (
     <animated.mesh
