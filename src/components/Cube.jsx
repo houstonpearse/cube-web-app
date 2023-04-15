6bdf37a5cd85e3e1ce6aaa2ee51a0f611dab0ab9
Houston Pearse <Houypee@gmail.com>
2023-04-16 00:49:14 +1000

    cubits animate independently

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -6,23 +6,28 @@ const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
 const tuples = [-1, 0, 1].flatMap(i => [-1, 0, 1].flatMap(j => [-1, 0, 1].map(k => [i, j, k])));
 
 export default function Cube() {
-  const rotation = useSpringValue([0, 0, 0], { config: springConfig });
-  const scale = useSpringValue(1, { config: springConfig });
-
-  const handleClick = () => {
+  const cubitProperties = tuples.map((position, index) => ({
+    id: index,
+    position: position,
+    rotation: useSpringValue([0, 0, 0], { config: springConfig }),
+    scale: useSpringValue(0.9, { config: springConfig }),
+  }));
+  console.log(cubitProperties)
+  const handleClick = (index) => {
     console.log("animate");
+    const {rotation, scale} = cubitProperties[index];
     rotation.start([randomAngle(), randomAngle(), randomAngle()]);
     scale.start(Math.random() + 0.5);
   };
   return (
   <group>
-    {tuples.map((position, index) => (
+    {cubitProperties.map((properties, index) => (
       <Cubit
         key={index}
-        rotation={rotation}
-        position={position}
-        scale={scale}
-        onClick={handleClick}
+        rotation={properties.rotation}
+        position={properties.position}
+        scale={properties.scale}
+        onClick={() => handleClick(index)}
       /> 
     ))}
   </group>
