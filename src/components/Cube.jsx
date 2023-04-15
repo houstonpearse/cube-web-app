408293a0bdacde88729407b95d63717a4715d506
Houston Pearse <Houypee@gmail.com>
2023-04-16 00:12:59 +1000

    add multiple cubits to form a cube

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,24 +3,28 @@ import Cubit from "./Cubit";
 
 const springConfig = { mass: 1, friction: 50, tension: 200 };
 const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
+const tuples = [-1, 0, 1].flatMap(i => [-1, 0, 1].flatMap(j => [-1, 0, 1].map(k => [i, j, k])));
 
 export default function Cube() {
   const rotation = useSpringValue([0, 0, 0], { config: springConfig });
-  const position = useSpringValue([0, 0, 0], { config: springConfig });
   const scale = useSpringValue(1, { config: springConfig });
 
   const handleClick = () => {
     console.log("animate");
     rotation.start([randomAngle(), randomAngle(), randomAngle()]);
-    position.start([Math.random(), Math.random(), Math.random()]);
     scale.start(Math.random() + 0.5);
   };
   return (
-    <Cubit
-      rotation={rotation}
-      position={position}
-      scale={scale}
-      onClick={() => handleClick()}
-    />
+  <group>
+    {tuples.map((position, index) => (
+      <Cubit
+        key={index}
+        rotation={rotation}
+        position={position}
+        scale={scale}
+        onClick={handleClick}
+      /> 
+    ))}
+  </group>
   );
 }
