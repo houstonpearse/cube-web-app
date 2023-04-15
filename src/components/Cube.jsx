657fc672e8daca92c1abfce3d8036b16e6892ff9
houy <67390187+houstonpearse@users.noreply.github.com>
2023-04-15 22:04:59 +1000

    used new cubit component

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
-import { animated, useSpring, useSpringValue } from "@react-spring/three";
+import { useSpringValue } from "@react-spring/three";
+import Cubit from "./Cubit";
 
-const defaultColors = ["red", "#f7aa36", "white", "yellow", "blue", "#6af752"];
 const springConfig = { mass: 1, friction: 50, tension: 200 };
 const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
 
-export default function Cube({
-  startingposition = [0, 0, 0],
-  startingrotation = [0, 0, 0],
-  startingscale = 1,
-  colors = defaultColors,
-}) {
-  const rotation = useSpringValue(startingrotation, { config: springConfig });
-  const position = useSpringValue(startingposition, { config: springConfig });
-  const scale = useSpringValue(startingscale, { config: springConfig });
+export default function Cube() {
+  const rotation = useSpringValue([0, 0, 0], { config: springConfig });
+  const position = useSpringValue([0, 0, 0], { config: springConfig });
+  const scale = useSpringValue(1, { config: springConfig });
 
   const handleClick = () => {
     console.log("animate");
@@ -22,22 +16,11 @@ export default function Cube({
     scale.start(Math.random() + 0.5);
   };
   return (
-    <animated.mesh
-      castShadow
-      receiveShadow
+    <Cubit
       rotation={rotation}
       position={position}
       scale={scale}
       onClick={() => handleClick()}
-    >
-      <boxGeometry />
-      {colors.map((color, index) => (
-        <meshStandardMaterial
-          key={index}
-          attach={`material-${index}`}
-          color={color}
-        />
-      ))}
-    </animated.mesh>
+    />
   );
 }
