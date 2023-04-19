ddd0137e522733a480e263079f771b8519b2b9b7
houston pearse <67390187+houstonpearse@users.noreply.github.com>
2023-04-20 08:31:56 +1000

    uses a cubit wrapped in animated

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,6 +1,7 @@
-import { useSpringValue } from "@react-spring/three";
+import { useSpringValue, animated } from "@react-spring/three";
 import Cubit from "./Cubit";
 
+const AnimatedCubit = animated(Cubit);
 const springConfig = { mass: 1, friction: 50, tension: 200 };
 const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
 const tuples = [-1, 0, 1].flatMap(i => [-1, 0, 1].flatMap(j => [-1, 0, 1].map(k => [i, j, k])));
@@ -8,21 +9,22 @@ const tuples = [-1, 0, 1].flatMap(i => [-1, 0, 1].flatMap(j => [-1, 0, 1].map(k 
 export default function Cube() {
   const cubitProperties = tuples.map((position, index) => ({
     id: index,
-    position: position,
+    position: useSpringValue(position, {config: springConfig}),
     rotation: useSpringValue([0, 0, 0], { config: springConfig }),
     scale: useSpringValue(0.9, { config: springConfig }),
   }));
   console.log(cubitProperties)
   const handleClick = (index) => {
     console.log("animate");
-    const {rotation, scale} = cubitProperties[index];
+    const {position, rotation, scale} = cubitProperties[index];
+    position.start([Math.random()*2 -1, Math.random()*2 -1, Math.random()*2 -1])
     rotation.start([randomAngle(), randomAngle(), randomAngle()]);
     scale.start(Math.random() + 0.5);
   };
   return (
   <group>
     {cubitProperties.map((properties, index) => (
-      <Cubit
+      <AnimatedCubit
         key={index}
         rotation={properties.rotation}
         position={properties.position}
