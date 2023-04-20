68fecb1737e4bc31483cc31842e29d6960886ea4
Houston Pearse <houypee@gmail.com>
2023-04-20 22:36:03 +1000

    updated cubit

--- src/components/Cubit.jsx
+++ src/components/Cubit.jsx
@@ -2,14 +2,7 @@ const colors = ["red", "#f7aa36", "white", "yellow", "blue", "#6af752"];
 
 export default function Cubit({ position, rotation, scale, ...rest }) {
   return (
-    <mesh
-      {...rest}
-      rotation={rotation}
-      position={position}
-      scale={scale}
-      castShadow
-      receiveShadow
-    >
+    <mesh {...rest} rotation={rotation} position={position} scale={scale}>
       <boxGeometry />
       {colors.map((color, index) => (
         <meshStandardMaterial
