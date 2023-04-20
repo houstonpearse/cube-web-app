312928b24eeb1694d9f900b142e251fe79a2aae8
Houston Pearse <houypee@gmail.com>
2023-04-20 22:36:53 +1000

    updated Cube to pass props

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -4,34 +4,41 @@ import Cubit from "./Cubit";
 const AnimatedCubit = animated(Cubit);
 const springConfig = { mass: 1, friction: 50, tension: 200 };
 const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
-const tuples = [-1, 0, 1].flatMap(i => [-1, 0, 1].flatMap(j => [-1, 0, 1].map(k => [i, j, k])));
+const tuples = [-1, 0, 1].flatMap((i) =>
+  [-1, 0, 1].flatMap((j) => [-1, 0, 1].map((k) => [i, j, k]))
+);
 
-export default function Cube() {
+export default function Cube(props) {
   const cubitProperties = tuples.map((position, index) => ({
     id: index,
-    position: useSpringValue(position, {config: springConfig}),
+    position: useSpringValue(position, { config: springConfig }),
     rotation: useSpringValue([0, 0, 0], { config: springConfig }),
     scale: useSpringValue(0.9, { config: springConfig }),
   }));
-  console.log(cubitProperties)
+  console.log(cubitProperties);
   const handleClick = (index) => {
     console.log("animate");
-    const {position, rotation, scale} = cubitProperties[index];
-    position.start([Math.random()*2 -1, Math.random()*2 -1, Math.random()*2 -1])
+    const { position, rotation, scale } = cubitProperties[index];
+    position.start([
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+    ]);
     rotation.start([randomAngle(), randomAngle(), randomAngle()]);
     scale.start(Math.random() + 0.5);
   };
   return (
-  <group>
-    {cubitProperties.map((properties, index) => (
-      <AnimatedCubit
-        key={index}
-        rotation={properties.rotation}
-        position={properties.position}
-        scale={properties.scale}
-        onClick={() => handleClick(index)}
-      /> 
-    ))}
-  </group>
+    <group>
+      {cubitProperties.map((properties, index) => (
+        <AnimatedCubit
+          {...props}
+          key={index}
+          rotation={properties.rotation}
+          position={properties.position}
+          scale={properties.scale}
+          onClick={() => handleClick(index)}
+        />
+      ))}
+    </group>
   );
 }
