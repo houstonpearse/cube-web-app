65e2aac0e9ad867b81f8026894650eab171695d3
Houston Pearse <Houypee@gmail.com>
2023-05-01 20:51:51 +1000

    use discrete rotations and positions

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,31 +1,33 @@
 import { useSpringValue, animated } from "@react-spring/three";
 import Cubit from "./Cubit";
+import {
+  eulerCubeRotations,
+  cubeRotationProductTable,
+  greekAlphabet,
+} from "../helpers/CubeRotation";
+import { cubePositions } from "../helpers/CubePostions";
 
 const AnimatedCubit = animated(Cubit);
 const springConfig = { mass: 1, friction: 50, tension: 200 };
-const randomAngle = () => Math.random() * Math.PI - Math.PI / 2;
-const tuples = [-1, 0, 1].flatMap((i) =>
-  [-1, 0, 1].flatMap((j) => [-1, 0, 1].map((k) => [i, j, k]))
-);
 
 export default function Cube(props) {
-  const cubitProperties = tuples.map((position, index) => ({
+  let cubitProperties = cubePositions.map((position, index) => ({
     id: index,
+    postitionIndex: index,
+    rotationIndex: 0,
     position: useSpringValue(position, { config: springConfig }),
-    rotation: useSpringValue([0, 0, 0], { config: springConfig }),
-    scale: useSpringValue(0.9, { config: springConfig }),
+    rotation: useSpringValue(eulerCubeRotations[0], { config: springConfig }),
+    scale: useSpringValue(0.93, { config: springConfig }),
   }));
-  console.log(cubitProperties);
   const handleClick = (index) => {
     console.log("animate");
-    const { position, rotation, scale } = cubitProperties[index];
-    position.start([
-      Math.random() * 2 - 1,
-      Math.random() * 2 - 1,
-      Math.random() * 2 - 1,
-    ]);
-    rotation.start([randomAngle(), randomAngle(), randomAngle()]);
-    scale.start(Math.random() + 0.5);
+    let { rotationIndex, position, rotation, scale } = cubitProperties[index];
+    rotationIndex++;
+    if (rotationIndex >= eulerCubeRotations.length) {
+      rotationIndex = 0;
+    }
+    cubitProperties[index].rotationIndex = rotationIndex;
+    rotation.start(eulerCubeRotations[rotationIndex]);
   };
   return (
     <group>
@@ -37,6 +39,8 @@ export default function Cube(props) {
           position={properties.position}
           scale={properties.scale}
           onClick={() => handleClick(index)}
+          castShadow
+          receiveShadow
         />
       ))}
     </group>
